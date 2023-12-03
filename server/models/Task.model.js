b528a168fea2a906b1f9f27f1844c71e7489715c
isin <jon.s124524@gmail.com>
2023-12-04 01:28:40 +0200

    feat: add task model

---
+++ server/models/Task.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      default: 'New'
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false
+    },
+    _deletedAt: {
+      type: Number
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+export default mongoose.model('Task', taskSchema)

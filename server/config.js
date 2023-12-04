6d727d78d088a77957e85a285f243a5471919d90
isin <jon.s124524@gmail.com>
2023-12-04 12:05:40 +0200

    feat: add mail activation func

--- server/config.js
+++ server/config.js
@@ -4,7 +4,11 @@ dotenv.config()
 
 const options = {
   port: process.env.PORT,
-  mongoUrl: process.env.MONGO_URL
+  mongoUrl: process.env.MONGO_URL,
+  smtpHost: process.env.SMTP_HOST,
+  smtpPort: process.env.SMTP_PORT,
+  smtpUser: process.env.SMTP_USER,
+  smtpPassword: process.env.SMTP_PASSWORD
 }
 
 export default options
---
+++ server/services/mailActivation.js
@@ -0,0 +1,29 @@
+import nodemailer from 'nodemailer'
+import options from '../config'
+
+const transporter = nodemailer.createTransport({
+  host: options.smtpHost,
+  port: options.smtpPort,
+  secure: false,
+  auth: {
+    user: options.smtpUser,
+    pass: options.smtpPassword
+  }
+})
+
+const sendActivationMail = async (to, link, name) => {
+  await transporter.sendMail({
+    from: options.smtpUser,
+    to,
+    subject: `${name}, welcome to the “To-Do List” App`,
+    html: (
+      <div>
+        <h1>
+          Hello, to activate your account, please follow the <a href={link}>Link</a>
+        </h1>
+      </div>
+    )
+  })
+}
+
+export default sendActivationMail

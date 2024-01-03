2d7da2a3d9022c4bf105ec0539453a6905979170
isin <jon.s124524@gmail.com>
2024-01-03 18:32:05 +0200

    fix

--- server/services/mailActivation.js
+++ server/services/mailActivation.js
@@ -12,17 +12,28 @@ const transporter = nodemailer.createTransport({
 })
 
 const sendActivationMail = async (to, code, name) => {
-  await transporter.sendMail({
-    from: options.smtpUser,
-    to,
-    subject: `${name}, welcome to the “To-Do List” App`,
-    html: `
-      <div>
-        <h1>
-          Hello, to activate your account, please enter this code: ${code}
-        </h1>
-      </div>
-    `
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(
+      {
+        from: options.smtpUser,
+        to,
+        subject: `${name}, welcome to the “To-Do List” App`,
+        html: `
+        <div>
+          <h1>
+            Hello, to activate your account, please enter this code: ${code}
+          </h1>
+        </div>
+      `
+      },
+      (error, info) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(info)
+        }
+      }
+    )
   })
 }
 
